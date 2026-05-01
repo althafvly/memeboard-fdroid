a33f094925747734aa73a78448dbdae9960e2e6b
Aarav Raut <bluetab410@gmail.com>
2026-05-01 08:34:37 +0530

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,8 +14,8 @@ android {
         applicationId = "com.bluefirestudios.memeboard"
         minSdk = 24
         targetSdk = 36
-        versionCode = 4
-        versionName = "2.1"
+        versionCode = 5
+        versionName = "2.2"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
