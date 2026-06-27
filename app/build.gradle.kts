af0dd37264524636c230051232f9c0412ff3d878
althafvly <althafvly@gmail.com>
2026-06-27 18:56:25 +0530

    Bump app version to 2.3

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,8 +14,8 @@ android {
         applicationId = "com.bluefirestudios.memeboard"
         minSdk = 24
         targetSdk = 36
-        versionCode = 5
-        versionName = "2.2"
+        versionCode = 6
+        versionName = "2.3"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
